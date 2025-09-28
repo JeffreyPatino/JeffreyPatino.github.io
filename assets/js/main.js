890ed1cc000ce242df099c3cfb6d467ca13d389b
Jeffrey Patino <jeffreyapatino@gmail.com>
2025-09-27 23:09:53 -0400

    chore: update url path

--- assets/js/main.js
+++ assets/js/main.js
@@ -146,7 +146,7 @@ document.getElementById('contact-form').addEventListener('submit', async functio
   const data = Object.fromEntries(formData.entries());
 
   try {
-    const response = await fetch('https://contact-form-handler.jeffreypatino.workers.dev', {
+    const response = await fetch('https://contact-form-handler.jeffreypatino.workers.dev/email', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
